f0a0cb23681215cfd7c3160edd2f75723d2f74bf
leesungkyu <lccab2002@gmail.com>
2022-06-05 00:56:43 +0900

    최신화

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, {Component} from "react";
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
 import './App.css';
 
+//랜덤 색상을 생성
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+
+  state ={
+    color : '#000000'
+  }
+
+  handleClick=()=>{
+    this.setState({
+      color: getRandomColor()
+    })
+  }
+
   render(){
     return(
-        <IterationSample/>
+        <div>
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <ErrorBoundary>
+            <LifeCycleSample color={this.state.color}/>
+          </ErrorBoundary>
+        </div>
     )
   }
 }
